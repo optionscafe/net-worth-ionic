f35dfb01b2ca220e63a7bcd0caac755031c7cc00
Spicer Matthews <github@cloudmanic.com>
2018-02-20 22:15:30 -0800

    More screwing around.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +7,53 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  isIos: boolean;
+  isCore: boolean;
+  isAndroid: boolean;
+
+  items = [
+    'Pokémon Yellow',
+    'Super Metroid',
+    'Mega Man X',
+    'The Legend of Zelda',
+    'Pac-Man',
+    'Super Mario World',
+    'Street Fighter II',
+    'Half Life',
+    'Final Fantasy VII',
+    'Star Fox',
+    'Tetris',
+    'Donkey Kong III',
+    'GoldenEye 007',
+    'Doom',
+    'Fallout',
+    'GTA',
+    'Halo'
+  ];
+
+
+  data = [
+    {
+      name: "AiA",
+      code: "AI101",
+      limit: 25000,
+      account: "Life Insurance"
+    },
+    {
+      name: "Cargills",
+      code: "CF001",
+      limit: 30000,
+      account: "Food City"
+    }
+  ]; 
+
+  constructor(public navCtrl: NavController, public platform: Platform) {
+
+    console.log(this.platform.platforms());
+
+    this.isIos = platform.is('ios');
+    this.isAndroid = platform.is('android');
+    this.isCore = platform.is('core');    
 
   }
 
